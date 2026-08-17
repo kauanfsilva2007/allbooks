d132f9cf7acfb7e542fefcf16a1e7537b2ec8c65
kauanfsilva2007 <kauanfsilva2007@gmail.com>
2026-08-17 09:19:34 -0300

    Altera server.js

--- allbooks-aula-01/src/server.js
+++ allbooks-aula-01/src/server.js
@@ -18,5 +18,6 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-  console.log("API disponível em http://localhost:8000")
+console.log("Boas vindas a API Allbooks")
+  console.log("API disponivel atraves da url http://localhost:8000")
 })
